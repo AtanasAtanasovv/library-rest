6b4a07251a5be8cdf8161ef983f92d632e34a29b
Vanya Staleva <vanya.staleva@asml.com>
2022-06-22 17:08:47 +0300

    update imports

--- library-rest/src/main/java/com/example/libraryrest/models/Book.java
+++ library-rest/src/main/java/com/example/libraryrest/models/Book.java
@@ -2,13 +2,22 @@ package com.example.libraryrest.models;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
 @Table(name = "books")
 @Data
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -17,12 +26,13 @@ public class Book {
     private String isbn;
     private String year;
     private String publisher;
-    @ManyToMany(cascade=CascadeType.PERSIST)
+    @ManyToMany(cascade= CascadeType.PERSIST)
     @JoinTable(
             name = "book_genre",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "genre_id"))
     private List<Genre> genres;
+
     @ManyToMany(cascade=CascadeType.PERSIST)
     @JoinTable(
             name = "book_author",
