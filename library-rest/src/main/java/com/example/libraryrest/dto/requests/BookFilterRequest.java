c953082ce4b7c3b84ceb79351cb39625bab06863
atanas.atanasov <atanas.atanasov@ultim8.io>
2022-07-21 14:43:37 +0300

    validations and constants added

--- library-rest/src/main/java/com/example/libraryrest/dto/requests/BookFilterRequest.java
+++ library-rest/src/main/java/com/example/libraryrest/dto/requests/BookFilterRequest.java
@@ -2,15 +2,22 @@ package com.example.libraryrest.dto.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Data
 public class BookFilterRequest {
+    @Pattern(regexp = "^[A-Z][a-z]*([-|\s][A-Z]?[a-z]*)*$")
     private String publisher;
+    @Pattern(regexp = "^[A-Z][a-z]*(([-|\\s][A-Z]?)?[a-z]+)*$")
     private String title;
+    @Pattern(regexp = "^1\\d{3}|(20(1[0-9]|2[0-2]|(0[0-9])))$")
     private Short yearFrom;
+    @Pattern(regexp = "^1\\d{3}|(20(1[0-9]|2[0-2]|(0[0-9])))$")
     private Short yearTo;
+    @Pattern(regexp = "^[A-Z][a-z]*(?:-[A-Z])?[a-z]*$")
     private String authorFirstName;
+    @Pattern(regexp = "^[A-Z][a-z]*(?:-[A-Z])?[a-z]*$")
     private String authorLastName;
     private String status;
 }
--- library-rest/src/main/java/com/example/libraryrest/specifications/BookSpecifications.java
+++ library-rest/src/main/java/com/example/libraryrest/specifications/BookSpecifications.java
@@ -4,39 +4,40 @@ import com.example.libraryrest.models.Author;
 import com.example.libraryrest.models.Book;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.Join;
 import javax.persistence.criteria.ListJoin;
-import java.util.List;
 
 public class BookSpecifications {
 
+    private static final String YEAR = "year";
+    private static final String PUBLISHER = "publisher";
+    private static final String TITLE = "title";
+    private static final String STATUS = "status";
+    private static final String AUTHORS = "authors";
+
+
     public static Specification<Book> yearFrom(Short yearFrom) {
-        return (root, query, builder) -> builder.greaterThanOrEqualTo(root.get("year"), yearFrom);
+        return (root, query, builder) -> builder.greaterThanOrEqualTo(root.get(YEAR), yearFrom);
     }
 
     public static Specification<Book> yearTo(Short yearTo) {
-        return (root, query, builder) -> builder.lessThanOrEqualTo(root.get("year"), yearTo);
+        return (root, query, builder) -> builder.lessThanOrEqualTo(root.get(YEAR), yearTo);
     }
 
     public static Specification<Book> publisherLike(String publisher) {
-        return (root, query, builder) -> builder.like(root.get("publisher"), "%" + publisher + "%");
+        return (root, query, builder) -> builder.like(root.get(PUBLISHER), "%" + publisher + "%");
     }
 
     public static Specification<Book> titleLike(String title) {
-        return (root, query, builder) -> builder.like(root.get("title"), "%" + title + "%");
+        return (root, query, builder) -> builder.like(root.get(TITLE), "%" + title + "%");
     }
 
     public static Specification<Book> checkStatus(String status) {
-        return (root, query, builder) -> builder.equal(root.get("status").as(String.class), status);
-    }
-
-    public static Specification<Book> betweenYears(Short yearFrom, Short yearTo) {
-        return (root, query, builder) -> builder.between(root.get("year"), yearFrom, yearTo);
+        return (root, query, builder) -> builder.equal(root.get(STATUS).as(String.class), status);
     }
 
     public static Specification<Book> authorName(String name, String fieldName) {
                 return (root, query, builder) -> {
-                    ListJoin<Book, Author> authorJoin = root.joinList("authors");
+                    ListJoin<Book, Author> authorJoin = root.joinList(AUTHORS);
                     return builder.in(authorJoin.get(fieldName)).value(name);
                 };
 
