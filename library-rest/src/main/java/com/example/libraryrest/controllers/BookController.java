c8debe15c79bc1671b80220961f500217b47b003
atanas.atanasov <atanas.atanasov@ultim8.io>
2022-08-04 16:26:28 +0300

    webclient test

--- library-rest/src/main/java/com/example/libraryrest/controllers/BookController.java
+++ library-rest/src/main/java/com/example/libraryrest/controllers/BookController.java
@@ -10,8 +10,12 @@ import com.example.libraryrest.dto.requests.UpdateYearRequest;
 import com.example.libraryrest.dto.requests.UpdatePublisherRequest;
 import com.example.libraryrest.dto.responses.BookFilterResponse;
 import com.example.libraryrest.dto.responses.BookResponse;
+import com.example.libraryrest.models.Student;
 import com.example.libraryrest.services.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +23,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
+import org.springframework.web.reactive.function.client.WebClient.UriSpec;
+import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -28,6 +36,18 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    @GetMapping("/student")
+    ResponseEntity<Student> getStudent() throws JsonProcessingException {
+        ObjectMapper objectMapper=new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        WebClient client=WebClient.create("http://localhost:8081");
+        UriSpec<RequestBodySpec> uriSpec = client.method(HttpMethod.GET);
+        RequestBodySpec bodySpec = uriSpec.uri("/student");
+        Mono<String> response = bodySpec.retrieve().bodyToMono(String.class);
+        Student student = objectMapper.readValue(response.block(),Student.class);
+        return ResponseEntity.ok(student);
+    }
 
     @GetMapping("/books")
     ResponseEntity<BookFilterResponse> filterBooks (BookFilterRequest filterRequest, PaginationRequest paginationRequest, SortRequest sortRequest){
